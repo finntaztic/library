59e9df60c8a03af1b49e81ca675dd129ad59f053
jttribiana <179311210+jttribiana@users.noreply.github.com>
2025-03-15 20:57:24 +0800

    Siomai

--- script.js
+++ script.js
@@ -2,20 +2,26 @@
 const myLibrary = [{
   title: "Babel",
   author: "RF Kuang",
+  id: crypto.randomUUID()
+  
 }, {
   title: "Kafka",
   author: "Murakami",
+  id: crypto.randomUUID()          
 }];
 
 
-function Book (title, author) {
+function Book (title, author, id = crypto.randomUUID()) {
   this.title = title;
   this.author = author;
+  this.id = id;
+
 }
 
 
-function addBookToLibrary(title, author) {
-  const newBook = new Book (title, author); 
+
+function addBookToLibrary(title, author, id) {
+  const newBook = new Book (title, author, id); 
   myLibrary.push (newBook);
   console.log(newBook)
   }
@@ -23,6 +29,7 @@ function addBookToLibrary(title, author) {
 
 // add the book title/author in the table
 const table = document.querySelector('.table');
+
 const tableBody = document.createElement ('tbody');
 table.appendChild(tableBody);
 
@@ -30,11 +37,18 @@ table.appendChild(tableBody);
 myLibrary.forEach(book => {
   let row = document.createElement ('tr');
 
+  
+
   Object.values(book).forEach(text => {
+    console.log(Object.values(book))
     let cell = document.createElement('td');
     let textNode = document.createTextNode(text); //inserts text to the td
-    cell.appendChild(textNode); //appending the text to the td
-    row.appendChild(cell); //append the td to the row
+
+      cell.appendChild(textNode); //appending the text to the td
+      row.appendChild(cell); //append the td to the row
+
+      
+    
   })
 
   tableBody.appendChild(row); // appends row to table body
@@ -44,16 +58,18 @@ myLibrary.forEach(book => {
 ///unresolved yet
 
 const button = document.createElement('button');
-const body = document.querySelector
+const body = document.querySelector ('body');
+body.appendChild(button);
+button.textContent = "New Book";
 
 
-const dialog = document.querySelector('dialog');
-const showBtn = document.querySelector('.newBook');
-const closeBtn = document.querySelector('dialog button');
+
+// const dialog = document.querySelector('dialog');
+// const closeBtn = document.querySelector('dialog button');
 
 
-showBtn.addEventListener('click', () => {
-  dialog.showModal();
-})
+// showBtn.addEventListener('click', () => {
+//   dialog.showModal();
+// })
 
 
