da2f0a5631cca7ee3fd63a145cf024054605b419
jttribiana <179311210+jttribiana@users.noreply.github.com>
2025-03-16 21:04:53 +0800

    add dialog modal

--- script.js
+++ script.js
@@ -49,3 +49,27 @@ myLibrary.forEach(book => {
   tableBody.appendChild(row); // appends row to table body
 
 })
+
+
+const newBtn = document.createElement('button');
+const body = document.querySelector('body');
+body.appendChild(newBtn);
+newBtn.textContent = 'New Button'
+
+const dialog = document.createElement('dialog')
+const closeBtn = document.createElement ('button')
+const p = document.createElement('p')
+p.textContent = "This modal dialog has a groovy backdrop!";
+body.appendChild(dialog)
+dialog.appendChild(closeBtn)
+dialog.appendChild(p)
+
+
+newBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeBtn.addEventListener("click", () => {
+  dialog.close();
+});
